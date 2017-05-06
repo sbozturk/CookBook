eefa4f272a66f4989ffbc6f9dfbec9a11c2bfca0
Satuk Buğrahan ÖZTÜRK <sbozturk1907@gmail.com>
2017-05-06 18:26:16 +0300

    PrefUtil class created

---
+++ app/src/main/java/com/bysoftware/cookbook/android/PrefUtil.java
@@ -0,0 +1,49 @@
+package com.bysoftware.cookbook.android;
+
+/**
+ * Created by sbozturk on 6.5.2017.
+ */
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class PrefUtil {
+
+    private Activity activity;
+
+    public PrefUtil(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void saveBio(String recipeName, String recipeIngredients, String recipeDirections, String recipePreparationTime) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("recipeName", recipeName);
+        editor.putString("recipeIngredients", recipeIngredients);
+        editor.putString("recipeDirections", recipeDirections);
+        editor.putString("recipePreparationTime", recipePreparationTime);
+        editor.apply();
+    }
+
+    public String getRecipeName() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        return prefs.getString("recipeName", null);
+    }
+
+    public String getRecipeIngredients() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        return prefs.getString("recipeIngredients", null);
+    }
+
+    public String getRecipeDirections() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        return prefs.getString("recipeDirections", null);
+    }
+
+    public String getRecipePreparationTime() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(activity);
+        return prefs.getString("recipePreparationTime", null);
+    }
+
+}
